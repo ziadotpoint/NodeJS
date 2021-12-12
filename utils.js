b60e8f7c84f5bafbb2d89e1d57201c385d1ada57
ziadotpoint <zia.mmd@gmail.com>
2021-12-12 12:32:50 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 /*todo: insert code here*/
 <html>
     <head></head>
 </html>
+=======
+todo: insert code here
+/* adding code to verify git conflict
+adding this content directly from git editor*/
+>>>>>>> 75e1c52 (Update utils.js)
